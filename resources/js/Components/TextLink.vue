0d576c4d6a22b38bb2dcc4080ddb805b5240cc7c
Hưng Võ <80050392+huwng12@users.noreply.github.com>
2024-11-21 17:01:42 +0700

    21/11/2024
    - Thiết lập lại project cho đơn giản
    - Hoàn thành các component cho 1 form cơ bản(register)

---
+++ resources/js/Components/TextLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    routeName: String,
+    label: String,
+})
+</script>
+<template>
+    <Link :href="route(routeName)"
+        class="font-medium underline text-indigo-500 hover:text-indigo-600 dark:text-indigo-400 dark:hover:text-indigo-500">
+    {{ label }}
+    </Link>
+</template>
